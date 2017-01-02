2f0865b02365cc7642e7b69fa5df9be7e7d7c5cd
Lorenz Koestler <lorenz@koestler.ch>
2017-01-02 21:43:44 +0100

    deepcopy device before returning it out of database

--- vedata/vedata.go
+++ vedata/vedata.go
@@ -1,15 +1,19 @@
 package vedata
 
 import (
+	"bytes"
+	"encoding/gob"
 	"errors"
 	"github.com/koestler/go-ve-sensor/bmv"
 	"log"
+	"time"
 )
 
 type DeviceId uint64
 
 type Device struct {
 	Name          string
+	LastUpdate    time.Time
 	NumericValues bmv.NumericValues
 }
 
@@ -30,7 +34,7 @@ type writeOp struct {
 	response      chan bool
 }
 
-type DbType map[DeviceId]Device
+type DbType map[DeviceId]*Device
 
 var running bool
 var db DbType
@@ -40,7 +44,7 @@ var writes chan *writeOp
 
 func init() {
 	running = false
-	db = make(map[DeviceId]Device)
+	db = make(map[DeviceId]*Device)
 
 	readDeviceChan = make(chan *readDeviceOp)
 	readDeviceIdsChan = make(chan *readDeviceIdsOp)
@@ -54,7 +58,7 @@ func CreateDevice(name string) (deviceId DeviceId) {
 
 	deviceId = DeviceId(len(db) + 1)
 
-	db[deviceId] = Device{
+	db[deviceId] = &Device{
 		Name:          name,
 		NumericValues: make(bmv.NumericValues),
 	}
@@ -93,22 +97,37 @@ func (deviceId DeviceId) Write(numericValues bmv.NumericValues) {
 	<-write.response
 }
 
+func clone(a, b interface{}) {
+	buff := new(bytes.Buffer)
+	enc := gob.NewEncoder(buff)
+	dec := gob.NewDecoder(buff)
+	enc.Encode(a)
+	dec.Decode(b)
+}
+
 func Run() {
 	go func() {
 		running = true
 		for {
 			select {
 			case write := <-writes:
-				for k, v := range write.numericValues {
-					db[write.deviceId].NumericValues[k] = v
+				device, ok := db[write.deviceId]
+				if ok {
+					for k, v := range write.numericValues {
+						device.NumericValues[k] = v
+					}
+					device.LastUpdate = time.Now()
 				}
 				write.response <- true
 			case read := <-readDeviceChan:
-				var ok bool
-				read.device, ok = db[read.deviceId]
+				device, ok := db[read.deviceId]
 				if !ok {
 					read.err = errors.New("device not found")
+				} else {
+					// make deep copy
+					clone(device, &read.device)
 				}
+
 				read.response <- true
 			case read := <-readDeviceIdsChan:
 				deviceIds := make([]DeviceId, len(db))
