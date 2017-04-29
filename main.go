259d7546017f98897de15f1657aefb4154e993a7
Lorenz Koestler <lorenz@koestler.ch>
2017-04-29 21:22:24 +0200

    reafactor main to accomodate for even more initialization

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"log"
 	"github.com/koestler/go-ve-sensor/dataflow"
+	"github.com/koestler/go-ve-sensor/config"
+	"github.com/koestler/go-ve-sensor/vehttp"
 )
+
+var rawStorage, roundedStorage dataflow.ValueStorageInstance
 
 func main() {
 	log.Print("main: start go-ve-sensor...")
 
+	setupStorageAndDataFlow()
+	setupBmvSources()
+	setupHttpServer()
+
+	log.Print("main: start completed; run until kill signal is received")
+
+	select {}
+}
+
+func setupStorageAndDataFlow() {
+	log.Printf("main: setup storage and data flow")
+
 	// setup dataflow pipeline
 	// 1. sources:
-	sources := BmvDevicesSetupAndRun()
+	// those are appended by separate routines
 
 	// 2. storage for raw values
 	rawStorage := dataflow.ValueStorageCreate()
@@ -22,22 +38,35 @@ func main() {
 	roundedStorage := dataflow.ValueStorageCreate()
 
 	// chain those
-	for _, source:= range sources {
-		source.Append(rawStorage)
-	}
 	rawStorage.Append(rounder)
 	rounder.Append(roundedStorage)
 
 	// setup some test sinks
 	devices := dataflow.DevicesGet()
-	f0 := dataflow.SubscriptionFilter{
-		Devices: map[*dataflow.Device]bool{devices[0]: true},
+	f0 := dataflow.Filter{
+		Devices:    map[*dataflow.Device]bool{devices[0]: true},
 		ValueNames: map[string]bool{"Power": true},
 	};
 	dataflow.SinkLog("raw    ", rawStorage.Subscribe(f0))
 	dataflow.SinkLog("rounded", roundedStorage.Drain())
 
-	log.Print("main: start completed")
-	select {}
+}
 
+func setupBmvSources() {
+	log.Printf("main: setup Bmv sources")
+
+	sources := BmvDevicesSetupAndRun()
+	for _, source := range sources {
+		source.Append(rawStorage)
+	}
+}
+
+func setupHttpServer() {
+	httpdConfig, err := config.GetHttpdConfig()
+	if err == nil {
+		log.Print("main: start http server, config=%v", httpdConfig)
+		go vehttp.Run(httpdConfig.Bind, httpdConfig.Port, HttpRoutes)
+	} else {
+		log.Printf("main: skip http server, err=%v", err)
+	}
 }
