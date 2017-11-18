885ef3b8e20c440c9b1e887aa4462cfbcac40623
Lorenz Koestler <lorenz@koestler.ch>
2017-11-18 22:03:34 +0100

    update victron solar charger definitions

--- vedirect/definitions.go
+++ vedirect/definitions.go
@@ -35,27 +35,29 @@ const (
 type VeProduct uint16
 
 const (
-	VeProductBmv700                    VeProduct = 0x0203
-	VeProductBmv702                    VeProduct = 0x0204
-	VeProductBmv700H                   VeProduct = 0x0205
-	VeProductBlueSolarMppt75_10        VeProduct = 0xA04C
-	VeProductBlueSolarMppt70_15        VeProduct = 0x0300
-	VeProductBlueSolarMppt75_15        VeProduct = 0xA042
-	VeProductBlueSolarMppt100_15       VeProduct = 0xA043
-	VeProductBlueSolarMppt100_30_rev1  VeProduct = 0xA044
-	VeProductBlueSolarMppt100_30_rev2  VeProduct = 0xA04A
-	VeProductBlueSolarMppt150_35_rev_1 VeProduct = 0xA041
-	VeProductBlueSolarMppt150_35_rev_2 VeProduct = 0xA04B
-	VeProductBlueSolarMppt150_45       VeProduct = 0xA04D
-	VeProductBlueSolarMppt75_50        VeProduct = 0xA040
-	VeProductBlueSolarMppt100_50_rev1  VeProduct = 0xA045
-	VeProductBlueSolarMppt100_50_rev2  VeProduct = 0xA049
-	VeProductBlueSolarMppt150_60       VeProduct = 0xA04E
-	VeProductBlueSolarMppt150_70       VeProduct = 0xA046
-	VeProductBlueSolarMppt150_85       VeProduct = 0xA04F
-	VeProductBlueSolarMppt150_100      VeProduct = 0xA047
-	VeProductSmartSolarMppt150_100     VeProduct = 0xA051
-	VeProductSmartSolarMppt250_100     VeProduct = 0xA050
+	VeProductBmv700                   VeProduct = 0x0203
+	VeProductBmv702                   VeProduct = 0x0204
+	VeProductBmv700H                  VeProduct = 0x0205
+	VeProductBlueSolarMppt70_15       VeProduct = 0x0300
+	VeProductBlueSolarMppt75_50       VeProduct = 0xA040
+	VeProductBlueSolarMppt150_35_rev1 VeProduct = 0xA041
+	VeProductBlueSolarMppt75_15       VeProduct = 0xA042
+	VeProductBlueSolarMppt100_15      VeProduct = 0xA043
+	VeProductBlueSolarMppt100_30_rev1 VeProduct = 0xA044
+	VeProductBlueSolarMppt100_50_rev1 VeProduct = 0xA045
+	VeProductBlueSolarMppt150_70      VeProduct = 0xA046
+	VeProductBlueSolarMppt150_100     VeProduct = 0xA047
+	VeProductBlueSolarMppt100_50_rev2 VeProduct = 0xA049
+	VeProductBlueSolarMppt100_30_rev2 VeProduct = 0xA04A
+	VeProductBlueSolarMppt150_35_rev2 VeProduct = 0xA04B
+	VeProductBlueSolarMppt75_10       VeProduct = 0xA04C
+	VeProductBlueSolarMppt150_45      VeProduct = 0xA04D
+	VeProductBlueSolarMppt150_60      VeProduct = 0xA04E
+	VeProductBlueSolarMppt150_85      VeProduct = 0xA04F
+	VeProductSmartSolarMppt250_100    VeProduct = 0xA050
+	VeProductSmartSolarMppt150_100    VeProduct = 0xA051
+	VeProductSmartSolarMppt150_85     VeProduct = 0xA052
+	VeProductSmartSolarMppt75_15      VeProduct = 0xA053
 )
 
 func (product VeProduct) String() string {
@@ -66,26 +68,46 @@ func (product VeProduct) String() string {
 		return "Bmv702"
 	case VeProductBmv700H:
 		return "Bmv700H"
-	case VeProductBlueSolarMppt75_10:
 	case VeProductBlueSolarMppt70_15:
-	case VeProductBlueSolarMppt75_15:
-	case VeProductBlueSolarMppt100_15:
-	case VeProductBlueSolarMppt100_30_rev1:
-	case VeProductBlueSolarMppt100_30_rev2:
-	case VeProductBlueSolarMppt150_35_rev_1:
-	case VeProductBlueSolarMppt150_35_rev_2:
-	case VeProductBlueSolarMppt150_45:
+		return "BlueSolarMppt70_15"
 	case VeProductBlueSolarMppt75_50:
+		return "BlueSolarMppt75_50"
+	case VeProductBlueSolarMppt150_35_rev1:
+		return "BlueSolarMppt150_35_rev1"
+	case VeProductBlueSolarMppt75_15:
+		return "BlueSolarMppt75_15"
+	case VeProductBlueSolarMppt100_15:
+		return "BlueSolarMppt100_15"
+	case VeProductBlueSolarMppt100_30_rev1:
+		return "BlueSolarMppt100_30_rev1"
 	case VeProductBlueSolarMppt100_50_rev1:
-	case VeProductBlueSolarMppt100_50_rev2:
-	case VeProductBlueSolarMppt150_60:
+		return "BlueSolarMppt100_50_rev1"
 	case VeProductBlueSolarMppt150_70:
-	case VeProductBlueSolarMppt150_85:
+		return "BlueSolarMppt150_70"
 	case VeProductBlueSolarMppt150_100:
-		return "BlueSolarMppt"
-	case VeProductSmartSolarMppt150_100:
+		return "BlueSolarMppt150_100"
+	case VeProductBlueSolarMppt100_50_rev2:
+		return "BlueSolarMppt100_50_rev2"
+	case VeProductBlueSolarMppt100_30_rev2:
+		return "BlueSolarMppt100_30_rev2"
+	case VeProductBlueSolarMppt150_35_rev2:
+		return "BlueSolarMppt150_35_rev2"
+	case VeProductBlueSolarMppt75_10:
+		return "BlueSolarMppt75_10"
+	case VeProductBlueSolarMppt150_45:
+		return "BlueSolarMppt150_45"
+	case VeProductBlueSolarMppt150_60:
+		return "BlueSolarMppt150_60"
+	case VeProductBlueSolarMppt150_85:
+		return "BlueSolarMppt150_85"
 	case VeProductSmartSolarMppt250_100:
-		return "SmartSolarMppt"
+		return "SmartSolarMppt250_100"
+	case VeProductSmartSolarMppt150_100:
+		return "SmartSolarMppt150_100"
+	case VeProductSmartSolarMppt150_85:
+		return "SmartSolarMppt150_85"
+	case VeProductSmartSolarMppt75_15:
+		return "SmartSolarMppt75_15"
 	}
 	return ""
 }
