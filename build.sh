8477cad07bd904c16576108dd962ca48a846c323
Lorenz Koestler <lorenz@koestler.ch>
2017-12-17 16:10:32 +0100

    fix frontend build

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+cd httpServer
+go generate
+cd -
+
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w'
--- frontend_to_bindata.sh
+++ frontend_to_bindata.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-go-bindata -prefix "../frontend/" -pkg webserver -nomemcopy -nocompress `find ../frontend/ -type d`
+go-bindata -prefix "../frontend/" -pkg httpServer -nomemcopy -nocompress `find ../frontend/ -type d`
