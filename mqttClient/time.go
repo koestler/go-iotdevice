058d552fb65c1b28b4e4ff638ee624c3d12a4241
Lorenz Koestler <lorenz@koestler.ch>
2022-02-06 23:28:27 +0100

    go fmt

--- mqttClient/time.go
+++ mqttClient/time.go
@@ -4,6 +4,6 @@ import "time"
 
 const timeFormat string = "2006-01-02T15:04:05"
 
-func timeToString(t time.Time) (string) {
+func timeToString(t time.Time) string {
 	return t.UTC().Format(timeFormat)
 }
