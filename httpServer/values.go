b3c3db06de425f63472e016bdb38d416ee3d2e73
Lorenz Koestler <lorenz@koestler.ch>
2022-03-27 14:05:40 +0200

    change websocket: send all data of view, aggregate changes values for a short period into one message

--- httpServer/values.go
+++ httpServer/values.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 	"log"
 	"net/http"
+	"time"
 )
 
 type valueResponse interface{}
@@ -45,7 +46,7 @@ func setupValuesJson(r *gin.RouterGroup, env *Environment) {
 					return
 				}
 				values := env.Storage.GetMap(deviceFilter)
-				jsonGetResponse(c, compileResponse(values))
+				jsonGetResponse(c, compile1DResponse(values))
 			})
 			if env.Config.LogConfig() {
 				log.Printf("httpServer: %s%s -> serve values as json", r.BasePath(), relativePath)
@@ -54,7 +55,7 @@ func setupValuesJson(r *gin.RouterGroup, env *Environment) {
 	}
 }
 
-func compileResponse(values dataflow.ValueMap) (response map[string]valueResponse) {
+func compile1DResponse(values dataflow.ValueMap) (response map[string]valueResponse) {
 	response = make(map[string]valueResponse, len(values))
 	for registerName, value := range values {
 		if numeric, ok := value.(dataflow.NumericRegisterValue); ok {
@@ -67,106 +68,151 @@ func compileResponse(values dataflow.ValueMap) (response map[string]valueRespons
 }
 
 // setupValuesWs godoc
-// @Summary Outputs the latest values of all the fields of a device.
+// @Summary Websocket that sends all values initially and sends updates of changed values subsequently.
 // @ID valuesWs
 // @Param viewName path string true "View name as provided by the config endpoint"
-// @Param deviceName path string true "Device name as provided in devices array of the config endpoint"
 // @Produce json
 // @success 200 {array} valueResponse
 // @Failure 404 {object} ErrorResponse
-// @Router /values/{viewName}/{deviceName}.ws [get]
+// @Router /values/{viewName}/ws [get]
 // @Security ApiKeyAuth
 func setupValuesWs(r *gin.RouterGroup, env *Environment) {
 	// add dynamic routes
 	for _, v := range env.Views {
 		view := v
-		for _, deviceName := range view.DeviceNames() {
+		relativePath := "values/" + view.Name() + "/ws"
+		deviceFilter := getDeviceFilter(view.DeviceNames())
 
-			device := env.DevicePoolInstance.GetDevice(deviceName)
-			if device == nil {
-				continue
+		// the follow line uses a loop variable; it must be outside the closure
+		r.GET(relativePath, func(c *gin.Context) {
+			// check authorization
+			if !isViewAuthenticated(view, c) {
+				log.Printf("httpServer: %s%s: permission denied", r.BasePath(), relativePath)
+				jsonErrorResponse(c, http.StatusForbidden, errors.New("User is not allowed here"))
+				return
 			}
 
-			relativePath := "values/" + view.Name() + "/" + deviceName + ".ws"
-
-			// the follow line uses a loop variable; it must be outside the closure
-			deviceFilter := dataflow.Filter{Devices: map[string]bool{deviceName: true}}
-			r.GET(relativePath, func(c *gin.Context) {
-				// check authorization
-				if !isViewAuthenticated(view, c) {
-					log.Printf("httpServer: %s%s: permission denied", r.BasePath(), relativePath)
-					jsonErrorResponse(c, http.StatusForbidden, errors.New("User is not allowed here"))
-					return
+			conn, _, _, err := ws.UpgradeHTTP(c.Request, c.Writer)
+			if err != nil {
+				log.Printf("httpServer: %s%s: error during upgrade: %s", r.BasePath(), relativePath, err)
+				if err := conn.Close(); err != nil {
+					log.Printf("httpServer: %s%s: error during close: %s", r.BasePath(), relativePath, err)
 				}
+				return
+			}
+			log.Printf("httpServer: %s%s: websocket connection established", r.BasePath(), relativePath)
 
-				conn, _, _, err := ws.UpgradeHTTP(c.Request, c.Writer)
-				if err != nil {
-					log.Printf("httpServer: %s%s: error during upgrade: %s", r.BasePath(), relativePath, err)
-					if err := conn.Close(); err != nil {
-						log.Printf("httpServer: %s%s: error during close: %s", r.BasePath(), relativePath, err)
-					}
-					return
-				}
-				log.Printf("httpServer: %s%s: websocket connection established", r.BasePath(), relativePath)
+			subscription := env.Storage.Subscribe(deviceFilter)
 
-				subscription := env.Storage.Subscribe(deviceFilter)
-
-				go func() {
-					defer subscription.Shutdown()
-					defer conn.Close()
-
-					for {
-						msg, op, err := wsutil.ReadClientData(conn)
-						if op == ws.OpClose {
-							log.Printf("httpServer: %s%s: close connection", r.BasePath(), relativePath)
-							return
-						}
-						if err != nil {
-							log.Printf("httpServer: %s%s: error during read: %s", r.BasePath(), relativePath, err)
-							return
-						} else {
-							log.Printf("httpServer: %s%s: message received: %s", r.BasePath(), relativePath, msg)
-						}
-					}
-				}()
-
-				go func() {
-					writer := wsutil.NewWriter(conn, ws.StateServerSide, ws.OpText)
-					encoder := json.NewEncoder(writer)
-
-					// send all values after initial connect
-					values := env.Storage.GetMap(deviceFilter)
-					response := compileResponse(values)
-					if err := wsSendResponse(writer, encoder, response); err != nil {
-						log.Printf("httpServer: %s%s: error while sending value: %s", r.BasePath(), relativePath, err)
+			go func() {
+				defer subscription.Shutdown()
+				defer conn.Close()
+				for {
+					msg, op, err := wsutil.ReadClientData(conn)
+					if op == ws.OpClose {
+						log.Printf("httpServer: %s%s: close connection", r.BasePath(), relativePath)
 						return
 					}
+					if err != nil {
+						log.Printf("httpServer: %s%s: error during read: %s", r.BasePath(), relativePath, err)
+						return
+					} else {
+						log.Printf("httpServer: %s%s: message received: %s", r.BasePath(), relativePath, msg)
+					}
+				}
+			}()
 
-					// send updates
-					for v := range subscription.GetOutput() {
-						// compile response
-						m := make(dataflow.ValueMap, 1)
-						m[v.Register().Name()] = v
-						response := compileResponse(m)
+			go func() {
+				writer := wsutil.NewWriter(conn, ws.StateServerSide, ws.OpText)
+				encoder := json.NewEncoder(writer)
 
-						// send response
-						if err := wsSendResponse(writer, encoder, response); err != nil {
-							log.Printf("httpServer: %s%s: error while sending value: %s", r.BasePath(), relativePath, err)
-							return
+				// send all values after initial connect
+				{
+					values := env.Storage.GetMap(deviceFilter)
+					response := compile2DResponse(values)
+					if err := wsSendResponse(writer, encoder, response); err != nil {
+						log.Printf("httpServer: %s%s: error while sending initial values: %s", r.BasePath(), relativePath, err)
+						return
+					}
+				}
+
+				// send updates
+				{
+					// rate limit number of sent messages to one message every 100ms
+					tickerDuration := 100 * time.Millisecond
+					ticker := time.NewTicker(tickerDuration)
+					tickerRunning := true
+					valuesC := subscription.GetOutput()
+					response := make(map[string]map[string]valueResponse, 1)
+					for {
+						select {
+						case <-ticker.C:
+							if len(response) > 0 {
+								// there is data to send, send it
+								if err := wsSendResponse(writer, encoder, response); err != nil {
+									log.Printf("httpServer: %s%s: error while sending value: %s", r.BasePath(), relativePath, err)
+									return
+								}
+								response = make(map[string]map[string]valueResponse, 1)
+							} else {
+								// no data to send; stop timer
+								ticker.Stop()
+								tickerRunning = false
+							}
+						case v, ok := <-valuesC:
+							if ok {
+								append2DResponseValue(response, v)
+								if !tickerRunning {
+									ticker.Reset(tickerDuration)
+									tickerRunning = true
+								}
+							} else {
+								// subscription was shutdown, stop
+								return
+							}
 						}
 					}
-					log.Printf("httpServer: %s%s: shutdown writing", r.BasePath(), relativePath)
-				}()
-			})
-			if env.Config.LogConfig() {
-				log.Printf("httpServer: %s%s -> setup websocket for values", r.BasePath(), relativePath)
-			}
+				}
+			}()
+		})
+		if env.Config.LogConfig() {
+			log.Printf("httpServer: %s%s -> setup websocket for values", r.BasePath(), relativePath)
 		}
 	}
 }
 
-func wsSendResponse(writer *wsutil.Writer, encoder *json.Encoder, response map[string]valueResponse) error {
-	log.Printf("websocket send: %v", response)
+func compile2DResponse(values dataflow.ValueMap) (response map[string]map[string]valueResponse) {
+	response = make(map[string]map[string]valueResponse, 1)
+	for _, value := range values {
+		append2DResponseValue(response, value)
+	}
+	return
+}
+
+func append2DResponseValue(response map[string]map[string]valueResponse, value dataflow.Value) {
+	d0 := value.DeviceName()
+	d1 := value.Register().Name()
+
+	if _, ok := response[d0]; !ok {
+		response[d0] = make(map[string]valueResponse, 1)
+	}
+
+	if numeric, ok := value.(dataflow.NumericRegisterValue); ok {
+		response[d0][d1] = numeric.Value()
+	} else if text, ok := value.(dataflow.TextRegisterValue); ok {
+		response[d0][d1] = text.Value()
+	}
+}
+
+func getDeviceFilter(deviceNames []string) dataflow.Filter {
+	devices := make(map[string]bool, len(deviceNames))
+	for _, deviceName := range deviceNames {
+		devices[deviceName] = true
+	}
+	return dataflow.Filter{Devices: devices}
+}
+
+func wsSendResponse(writer *wsutil.Writer, encoder *json.Encoder, response map[string]map[string]valueResponse) error {
 	if err := encoder.Encode(&response); err != nil {
 		return err
 	}
