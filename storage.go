d01f2773548444cc997d2ad4b7498f1a8664dbcd
Lorenz Koestler <lorenz@koestler.ch>
2023-03-13 22:25:01 +0100

    do not log storage start

--- storage.go
+++ storage.go
@@ -3,14 +3,9 @@ package main
 import (
 	"github.com/koestler/go-iotdevice/config"
 	"github.com/koestler/go-iotdevice/dataflow"
-	"log"
 )
 
 func runStorage(cfg *config.Config) *dataflow.ValueStorageInstance {
-	if cfg.LogWorkerStart() {
-		log.Printf("storage: setup rawStorage")
-	}
-
 	storageInstance := dataflow.ValueStorageCreate()
 
 	if cfg.LogStorageDebug() {
