bb6506b80d82bbbdd9aae909013ebb21c1d009d0
Lorenz Koestler <lorenz@koestler.ch>
2023-08-23 13:55:25 +0200

    make queue thread-safe and remove len function

--- queue/fifo.go
+++ queue/fifo.go
@@ -2,34 +2,37 @@ package queue
 
 import (
 	"container/list"
+	"sync"
 )
 
 type FifoQueue[T any] struct {
 	maxLength     int
 	containerList list.List
+	lock          sync.Mutex
 }
 
 func NewFifoQueue[T any](maxLength int) FifoQueue[T] {
 	return FifoQueue[T]{maxLength: maxLength}
 }
 
-func (q *FifoQueue[T]) Len() int {
-	return q.containerList.Len()
-}
-
 func (q *FifoQueue[T]) Enqueue(value T) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	q.containerList.PushBack(value)
 
 	// when list gets to long; truncate first element
 	if q.containerList.Len() > q.maxLength {
-		elem := q.containerList.Front()
-		if elem != nil {
+		if elem := q.containerList.Front(); elem != nil {
 			q.containerList.Remove(elem)
 		}
 	}
 }
 
 func (q *FifoQueue[T]) Dequeue() (value T, ok bool) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	elem := q.containerList.Front()
 	if elem == nil {
 		var empty T
