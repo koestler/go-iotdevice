dd00b506ac4145ec261ae3111ad445fd16304afa
Lorenz Koestler <lorenz@koestler.ch>
2024-11-27 20:50:18 +0100

    rename AirIntake/Exhaust temp to AuxTemp[01]

--- generator/string.go
+++ generator/string.go
@@ -5,10 +5,10 @@ import "fmt"
 func (i Inputs) String() string {
 	return fmt.Sprintf(
 		"Inputs{Time: %v, CommandSwitch: %v, ResetSwitch: %v, "+
-			"IOAvailable: %v, ArmSwitch: %v, FireDetected: %v, EngineTemp: %v, AirIntakeTemp: %v, AirExhaustTemp: %v, "+
+			"IOAvailable: %v, ArmSwitch: %v, FireDetected: %v, EngineTemp: %v, AuxTemp0: %v, AuxTemp1: %v, "+
 			"OutputAvailable: %v, U0: %v, U1: %v, U2: %v, L0: %v, L1: %v, L2: %v, F: %v}",
 		i.Time, i.CommandSwitch, i.ResetSwitch,
-		i.IOAvailable, i.ArmSwitch, i.FireDetected, i.EngineTemp, i.AirIntakeTemp, i.AirExhaustTemp,
+		i.IOAvailable, i.ArmSwitch, i.FireDetected, i.EngineTemp, i.AuxTemp0, i.AuxTemp1,
 		i.OutputAvailable, i.U0, i.U1, i.U2, i.L0, i.L1, i.L2, i.F,
 	)
 }
